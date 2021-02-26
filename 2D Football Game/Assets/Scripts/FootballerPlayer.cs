90a69175baa6e6ad4f86a1aabaa4be2f8e1ade47
neiltonna <neil9096@gmail.com>
2021-02-26 15:33:15 +0100

    Initial Commit
    Created new repository, created and done some basic things such as importing images and also created a script.

---
+++ 2D Football Game/Assets/Scripts/FootballerPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootballerPlayer : MonoBehaviour
+{
+    [SerializeField] float speedOfMovement = 6f;
+    float padding = 0.4f;
+    float xLower;
+    float xHigher;
+    float yLower;
+    float yHigher;
+
+    void Start()
+    {
+        borderOfCamera();
+    }
+
+    void Update()
+    {
+        footballerMovement();
+    }
+
+    void footballerMovement()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * speedOfMovement;
+        var newXPosition = Mathf.Clamp(transform.position.x + deltaX, xLower, xHigher);
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * speedOfMovement;
+        var newYPosition = Mathf.Clamp(transform.position.y + deltaY, yLower, yHigher);
+
+        transform.position = new Vector3(newXPosition, newYPosition, transform.position.z);
+    }
+
+    void borderOfCamera()
+    {
+        Camera gameCamera = Camera.main;
+        xLower = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xHigher = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yLower = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yHigher = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+}
